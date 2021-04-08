747e9672cc785d5c7db9fbc2f0d2169f465ab1e2
nb2904 <nusrat.ismoilovnb@gmail.com>
2021-04-09 06:44:06 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

---
+++ utils.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+
+
+def data_gen(path_to_images, data_type, path_save):
+    img_names = os.listdir(path_to_images)
+    img_names.sort()
+    data = open(path_save+data_type+".txt", "w")
+    for i in range(len(img_names)):
+        img = cv2.imread(path_to_images + img_names[i])
+        img_h = img.shape[0]
+        img_w = img.shape[1]
+        count_w = int(img_w / 32)
+        count_h = int(img_h / 16)
+        for w in range(count_w):
+            for h in range(count_h):
+                line = img_names[i]+" " + "w:"+str(w)+" h:"+str(h)+"\n"
+                data.write(line)
+    data.close()
