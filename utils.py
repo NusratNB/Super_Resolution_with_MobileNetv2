4935c6ff77b3717c2b28e65c82bcc2889a731c9b
nb2904 <nusrat.ismoilovnb@gmail.com>
2021-04-10 01:05:34 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 import os
 import cv2
+import tensorflow as tf
 
 
 def data_gen(path_to_images, data_type, path_save):
     img_names = os.listdir(path_to_images)
     img_names.sort()
-    data = open(path_save+data_type+".txt", "w")
+    data = open(path_save + data_type + ".txt", "w")
     for i in range(len(img_names)):
         img = cv2.imread(path_to_images + img_names[i])
         img_h = img.shape[0]
@@ -14,6 +15,10 @@ def data_gen(path_to_images, data_type, path_save):
         count_h = int(img_h / 16)
         for w in range(count_w):
             for h in range(count_h):
-                line = img_names[i]+" " + "w:"+str(w)+" h:"+str(h)+"\n"
+                line = img_names[i] + " " + "w:" + str(w) + " h:" + str(h) + "\n"
                 data.write(line)
     data.close()
+
+
+def ssim_loss(predicted, gt, max_val=1.0):
+    return 1 - tf.reduce_mean(tf.image.ssim(predicted, gt, max_val=max_val))
