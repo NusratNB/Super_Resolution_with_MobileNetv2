7cf24bd7cc39f0049ea9406e77dc1acd8fb4c534
nb2904 <nusrat.ismoilovnb@gmail.com>
2021-04-20 07:59:43 +0900

    Update

--- utils.py
+++ utils.py
@@ -11,8 +11,8 @@ def data_gen(path_to_images, data_type, path_save):
         img = cv2.imread(path_to_images + img_names[i])
         img_h = img.shape[0]
         img_w = img.shape[1]
-        count_w = int(img_w / 32)
-        count_h = int(img_h / 16)
+        count_w = int(img_w / 128)
+        count_h = int(img_h / 64)
         for w in range(count_w):
             for h in range(count_h):
                 line = img_names[i] + "_"+str(w) + "_" + str(h) + "\n"
